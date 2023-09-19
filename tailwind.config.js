c31a388b40b4b7fda6e611d7e8fa2faf5ebcabfa
jade-arinal-banares <jade.arinal.banares@gmail.com>
2023-09-19 09:42:02 +0800

    rearrange stuff

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: ["./**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        colorBlack: "#0B0D17",
+        paleBlue: "#D0D6F9",
+        colorWhite: "#FFFFFF",
+      },
+      fontFamily: {
+        barlowCondensed: ["BarlowCondensed-Regular", "sans-serif"],
+        belleFair: ["Bellefair-Regular", "serif"],
+      },
+    },
   },
   plugins: [],
-}
+};
